95de91df90bc97df97b5cb6b724390e7b620f688
unknown <listoria44@gmail.com>
2026-02-19 21:17:16 +0300

    second commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """Renders the main page of the website."""
-    return render_template('cekici_sitesi.html')
+    return render_template('index.html')
 
 @app.route("/galeri")
 def galeri():
@@ -13,4 +13,4 @@ def galeri():
     return render_template('galeri.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
