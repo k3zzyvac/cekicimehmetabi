bcaadd26bc77736a7f39b2f0254e785daa098362
unknown <listoria44@gmail.com>
2026-02-19 18:11:25 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    """Renders the main page of the website."""
+    return render_template('cekici_sitesi.html')
+
+@app.route("/galeri")
+def galeri():
+    """Renders the gallery page of the website."""
+    return render_template('galeri.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
